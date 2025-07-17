efbfc2e539f9d0b653c1b35b99b6027932259925
MohammadFH <mohamedalfahham@gmail.com>
2025-07-17 18:54:33 +0300

    message

--- productSurvey/product_survey.js
+++ productSurvey/product_survey.js
@@ -1,13 +1,13 @@
-const username = document.getElementById('name').value;
+
+function submitFeedback()
+{
+    const username = document.getElementById('name').value;
 const age = document.getElementById('age').value;
 const email = document.getElementById('email').value;
 const job = document.getElementById('job').value;
 const designation = document.getElementById('designation').value;
 const productType = document.getElementById('productType').value;
 const feedback = document.getElementById('feedbackText').value;
-
-function submitFeedback()
-{
 
     alert("Thank you for your valuable feedback");
 
