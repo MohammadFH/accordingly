e93f634b48e886dd7457fabe6a6b1fb523b2e918
MohammadFH <mohamedalfahham@gmail.com>
2025-07-21 22:32:58 +0300

    message

---
+++ DebugCode/debug_code.js
@@ -0,0 +1,29 @@
+function performOperation()
+{
+    let num1 = document.getElementById('input1').value;
+    let num2 = document.getElementById('input2').value;
+
+    if(!isNaN(num1) && !isNaN(num2))
+    {
+        let result = multiply(num1 , num2);
+        
+        displayResult(result);
+    }
+    else
+    {
+        displayResult("Please enter a valid numbers");
+    }
+}
+
+function multiply(a , b)
+{
+    debugger;
+
+    return a*b;
+}
+
+function displayResult(result)
+{
+    const resultElement = document.getElementById('result');
+    resultElement.textContent = `The result is: ${result}`;
+}
